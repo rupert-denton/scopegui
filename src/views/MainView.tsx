bc4d96178ccb018f2351c13f142e213bca5a079b
Ethan Fraser <ejpfraser@gmail.com>
2024-09-05 18:55:05 +1200

    render levels as list

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -3,11 +3,22 @@ import useScopeAndSequence from "../hooks/useScopeAndSequence";
 export default function MainView() {
   const { scopeAndSequence } = useScopeAndSequence();
 
-  if (!scopeAndSequence)
+  if (!scopeAndSequence) {
     return (
       <>
         <p>No scope and sequence loaded.</p>
         <FileSelector />
       </>
     );
+  }
+
+  return (
+    <ul>
+      {scopeAndSequence.data.map((item) => (
+        <li key={item.id}>
+          {item.level} - {item.levelInfo}
+        </li>
+      ))}
+    </ul>
+  );
 }
