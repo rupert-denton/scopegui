3cbb482e9da2334e3fff7e337b4018917e5d7cba
Ethan Fraser <ejpfraser@gmail.com>
2024-09-05 19:32:42 +1200

    whitespace

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,6 +1,7 @@
 import FileSelector from "../components/FileSelector";
 import useScopeAndSequence from "../hooks/useScopeAndSequence";
 import LevelList from "../components/LevelList";
+
 export default function MainView() {
   const { scopeAndSequence } = useScopeAndSequence();
 
