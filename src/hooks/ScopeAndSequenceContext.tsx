be228317ca1529169932aeb4b93eea72a876795e
Ethan Fraser <ejpfraser@gmail.com>
2024-09-05 14:51:55 +1200

    save state to local storage

--- src/hooks/ScopeAndSequenceContext.tsx
+++ src/hooks/ScopeAndSequenceContext.tsx
@@ -19,9 +19,26 @@ interface ScopeAndSequenceProviderProps {
 export function ScopeAndSequenceProvider({
   children,
 }: ScopeAndSequenceProviderProps) {
-  const [scopeAndSequence, setScopeAndSequence] =
-    useState<ScopeAndSequence | null>(null);
+  let localStorageItem: ScopeAndSequence | null = null;
+  try {
+    const storedItem = localStorage.getItem("scopeAndSequence");
+    localStorageItem = storedItem ? JSON.parse(storedItem) : null;
+  } catch (error) {
+    console.error("Error parsing scope and sequence from localStorage:", error);
+  }
+  const [scopeAndSequence, _setScopeAndSequence] =
+    useState<ScopeAndSequence | null>(localStorageItem);
+
   const [selectedLevel, setSelectedLevel] = useState(0);
+
+  function setScopeAndSequence(data: ScopeAndSequence | null) {
+    if (data) {
+      localStorage.setItem("scopeAndSequence", JSON.stringify(data));
+    } else {
+      localStorage.removeItem("scopeAndSequence");
+    }
+    _setScopeAndSequence(data);
+  }
 
   function loadScopeAndSequence(file: File) {
     const reader = new FileReader();
