c63ea9fca4d9f8e572f82684514f6f11bec71884
Ethan Fraser <ejpfraser@gmail.com>
2024-09-05 18:50:42 +1200

    set up main layout

--- src/views/MainLayout.tsx
+++ src/views/MainLayout.tsx
@@ -1,10 +1,47 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import useScopeAndSequence from "../hooks/useScopeAndSequence";
+import VersionSelector from "../components/VersionSelector";
 
 export default function MainLayout() {
+  const { scopeAndSequence, saveScopeAndSequence, unloadScopeAndSequence } =
+    useScopeAndSequence();
+
   return (
-    <div>
-      <h1>Scope And Sequence Editor</h1>
+    <MainLayoutContainer>
+      <Header>
+        <h1>Scope And Sequence Editor</h1>
+        {scopeAndSequence && (
+          <StatusContainer>
+            <VersionSelector />
+            <button onClick={saveScopeAndSequence}>Save</button>
+            <button onClick={unloadScopeAndSequence}>Unload</button>
+          </StatusContainer>
+        )}
+      </Header>
       <Outlet />
-    </div>
+    </MainLayoutContainer>
   );
 }
+
+const MainLayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 2rem;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StatusContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,21 +1,13 @@
 import FileSelector from "../components/FileSelector";
-import VersionSelector from "../components/VersionSelector";
 import useScopeAndSequence from "../hooks/useScopeAndSequence";
 export default function MainView() {
-  const { scopeAndSequence, saveScopeAndSequence, unloadScopeAndSequence } =
-    useScopeAndSequence();
+  const { scopeAndSequence } = useScopeAndSequence();
 
-  return scopeAndSequence ? (
-    <>
-      <p>Scope and Sequence loaded</p>
-      <VersionSelector />
-      <button onClick={saveScopeAndSequence}>Save</button>
-      <button onClick={unloadScopeAndSequence}>Unload</button>
-    </>
-  ) : (
-    <>
-      <p>No scope and sequence loaded.</p>
-      <FileSelector />
-    </>
-  );
+  if (!scopeAndSequence)
+    return (
+      <>
+        <p>No scope and sequence loaded.</p>
+        <FileSelector />
+      </>
+    );
 }
