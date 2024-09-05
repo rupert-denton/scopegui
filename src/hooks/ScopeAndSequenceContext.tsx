c8afd9fcd0bab04d18dce4895d3ad42636239a22
Ethan Fraser <ejpfraser@gmail.com>
2024-09-05 21:20:27 +1200

    save updates with version number

--- src/hooks/ScopeAndSequenceContext.tsx
+++ src/hooks/ScopeAndSequenceContext.tsx
@@ -64,12 +64,15 @@ export function ScopeAndSequenceProvider({
   }
 
   function saveScopeAndSequence() {
+    const version = updatedScopeAndSequence?.version;
     const data = JSON.stringify(updatedScopeAndSequence, null, 2);
     const blob = new Blob([data], { type: "application/json" });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
-    a.download = "scopeAndSequence.json";
+    a.download = version
+      ? `scopeAndSequence-${version}.json`
+      : "scopeAndSequence-updated.json";
     a.click();
   }
 
