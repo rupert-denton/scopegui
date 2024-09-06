b0f3cb827c056b4d75266a455f032fa02ef336b4
Ethan Fraser <ejpfraser@gmail.com>
2024-09-06 22:28:41 +1200

    huge tidy up of GameData component

--- src/components/GameData.tsx
+++ src/components/GameData.tsx
@@ -1,12 +1,5 @@
 import styled from "styled-components";
-import {
-  ScopeAndSequenceLevel,
-  Code,
-  Morpheme,
-  WordItem,
-  MorphemeWord,
-  SentenceItem,
-} from "../model.ts";
+import { ScopeAndSequenceLevel } from "../model.ts";
 import GameDataItem from "./GameDataItem.tsx";
 import GameDataSheet from "./GameDataSheet.tsx";
 
@@ -14,11 +7,13 @@ interface GameDataProps {
   fieldName: keyof ScopeAndSequenceLevel;
   levelData: ScopeAndSequenceLevel;
   updateLevel: (updatedLevel: ScopeAndSequenceLevel) => void;
+  showAddButton?: boolean;
 }
 export default function GameData({
   fieldName,
   levelData,
   updateLevel,
+  showAddButton = false,
 }: GameDataProps) {
   const renderField = () => {
     if (!levelData[fieldName] || !Array.isArray(levelData[fieldName])) {
@@ -28,113 +23,53 @@ export default function GameData({
     switch (fieldName) {
       case "newCode":
       case "cumulativeCode":
-        return renderCodeList(levelData[fieldName] as Code[]);
+        return renderItemList(levelData[fieldName], "spelling");
 
       case "newMorphemes":
       case "cumulativeMorphemes":
-        return renderMorphemeList(levelData[fieldName] as Morpheme[]);
+        return renderItemList(levelData[fieldName], "morpheme");
 
       case "wordSets":
       case "wordChains":
-        return renderWordItemList(levelData[fieldName] as WordItem[]);
+        return renderItemList(levelData[fieldName], "word");
 
       case "morphemeWordSets":
-        return renderMorphemeWordList(levelData[fieldName] as MorphemeWord[]);
+        return renderItemList(levelData[fieldName], "word");
 
       case "sentences":
-        return renderSentenceList(levelData[fieldName] as SentenceItem[]);
+        return renderItemList(levelData[fieldName], "sentence");
 
       case "trickyWords":
-        return renderTrickyWords(levelData[fieldName] as string[]);
+        return renderItemList(levelData[fieldName]);
 
       default:
         return <p>Unsupported field</p>;
     }
   };
 
-  const renderCodeList = (codes: Code[]) =>
-    codes.map((code, index) => (
-      <GameDataSheet
-        key={index}
-        fieldName={fieldName}
-        item={code}
-        index={index}
-        levelData={levelData}
-        updateLevel={updateLevel}
-      >
-        <GameDataItem value={code.spelling} />
-      </GameDataSheet>
-    ));
-
-  const renderMorphemeList = (morphemes: Morpheme[]) =>
-    morphemes.map((morpheme, index) => (
-      <GameDataSheet
-        key={index}
-        fieldName={fieldName}
-        item={morpheme}
-        index={index}
-        levelData={levelData}
-        updateLevel={updateLevel}
-      >
-        <GameDataItem value={morpheme.morpheme} />
-      </GameDataSheet>
-    ));
-
-  const renderWordItemList = (words: WordItem[]) =>
-    words.map((wordItem, index) => (
-      <GameDataSheet
-        key={index}
-        fieldName={fieldName}
-        item={wordItem}
-        index={index}
-        levelData={levelData}
-        updateLevel={updateLevel}
-      >
-        <GameDataItem value={wordItem.word} />
-      </GameDataSheet>
-    ));
-
-  const renderMorphemeWordList = (morphemeWords: MorphemeWord[]) =>
-    morphemeWords.map((mw, index) => (
-      <GameDataSheet
-        key={index}
-        fieldName={fieldName}
-        item={mw}
-        index={index}
-        levelData={levelData}
-        updateLevel={updateLevel}
-      >
-        <GameDataItem value={mw.word} />
-      </GameDataSheet>
-    ));
-
-  const renderSentenceList = (sentences: SentenceItem[]) =>
-    sentences.map((sentenceItem, index) => (
-      <GameDataSheet
-        key={index}
-        fieldName={fieldName}
-        item={sentenceItem}
-        index={index}
-        levelData={levelData}
-        updateLevel={updateLevel}
-      >
-        <GameDataItem value={sentenceItem.sentence} />
-      </GameDataSheet>
-    ));
-
-  const renderTrickyWords = (words: string[]) =>
-    words.map((word, index) => (
-      <GameDataSheet
-        key={index}
-        fieldName={fieldName}
-        item={word}
-        index={index}
-        levelData={levelData}
-        updateLevel={updateLevel}
-      >
-        <GameDataItem value={word} />
-      </GameDataSheet>
-    ));
+  function renderItemList<T>(items: T[], displayKey?: string) {
+    return (
+      <>
+        {items.map((item, index) => (
+          <GameDataSheet
+            key={index}
+            fieldName={fieldName}
+            item={item}
+            index={index}
+            levelData={levelData}
+            updateLevel={updateLevel}
+          >
+            <GameDataItem
+              value={
+                (displayKey ? item[displayKey as keyof T] : item) as string
+              }
+            />
+          </GameDataSheet>
+        ))}
+        {showAddButton && <GameDataItem value="+" />}
+      </>
+    );
+  }
 
   return <GameDataContainer>{renderField()}</GameDataContainer>;
 }
